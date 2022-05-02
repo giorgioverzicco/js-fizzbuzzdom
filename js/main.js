56376451404f07e532d4abf089dabcd60bfabdee
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-05-02 15:21:56 +0200

    add ui

--- js/main.js
+++ js/main.js
@@ -13,3 +13,25 @@ for (let i = 1; i <= 100; i++) {
 
 // BONUS ONE-LINER
 for (let i = 0; i++ < 100; console.log((i % 3 ? "" : "Fizz") + (i % 5 ? "" : "Buzz") || i));
+
+// MILESTONE 2-3
+const row = document.querySelector(".row");
+
+for (let i = 1; i <= 100; i++) {
+  const col = document.createElement("div");
+  col.classList.add("col");
+  row.append(col);
+
+  if (i % 3 === 0 && i % 5 === 0) {
+    col.classList.add("fizz-buzz");
+    col.innerText = "FizzBuzz";
+  } else if (i % 3 === 0) {
+    col.classList.add("fizz");
+    col.innerText = "Fizz";
+  } else if (i % 5 === 0) {
+    col.classList.add("buzz");
+    col.innerText = "Buzz";
+  } else {
+    col.innerText = i;
+  }
+}
