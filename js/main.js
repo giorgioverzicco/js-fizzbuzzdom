e9ab46a77651da7e201b3a0184524b70eddf3c3d
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-05-03 09:25:49 +0200

    refactor

--- js/main.js
+++ js/main.js
@@ -1,20 +1,7 @@
-// MILESTONE 1
-for (let i = 1; i <= 100; i++) {
-  if (i % 3 === 0 && i % 5 === 0) {
-    console.log("FizzBuzz");
-  } else if (i % 3 === 0) {
-    console.log("Fizz");
-  } else if (i % 5 === 0) {
-    console.log("Buzz");
-  } else {
-    console.log(i);
-  }
-}
-
-// BONUS ONE-LINER
+/* BONUS ONE-LINER
 for (let i = 0; i++ < 100; console.log((i % 3 ? "" : "Fizz") + (i % 5 ? "" : "Buzz") || i));
+*/
 
-// MILESTONE 2-3
 const row = document.querySelector(".row");
 
 for (let i = 1; i <= 100; i++) {
@@ -23,15 +10,19 @@ for (let i = 1; i <= 100; i++) {
   row.append(col);
 
   if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
     col.classList.add("fizz-buzz");
     col.innerText = "FizzBuzz";
   } else if (i % 3 === 0) {
+    console.log("Fizz");
     col.classList.add("fizz");
     col.innerText = "Fizz";
   } else if (i % 5 === 0) {
+    console.log("Buzz");
     col.classList.add("buzz");
     col.innerText = "Buzz";
   } else {
+    console.log(i);
     col.innerText = i;
   }
 }
